4ea6072ecef7a546693d88190fd0c60fe98cf61d
Luke Yue <lukedyue@gmail.com>
2019-09-07 14:36:01 +0800

    Add arm-linux-gnueabi GCC 9.2.0 toolchain for x86_64 hosts
    This is a GCC 9.2.0 toolchain compiled using Nathan Chancellor's build
    script: https://github.com/nathanchance/scripts/blob/master/funcs/bldgcc
    
    Signed-off-by: Luke Yue <lukedyue@gmail.com>

---
+++ lib/gcc/arm-linux-gnueabi/9.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/luke/aosp/build-gcc/gcc/build/gcc-9.2.0/configure --target=arm-linux-gnueabi --enable-targets=all --prefix=/home/luke/aosp/build-gcc/gcc/9.2.0 --enable-languages=c --without-headers --disable-bootstrap --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
